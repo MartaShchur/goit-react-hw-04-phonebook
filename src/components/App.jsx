2cd7e560be67e3d2c0943e898a26b75977e43b3b
MartaShchur <m.matseyko199@gmail.com>
2023-10-23 13:17:57 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
 
-const phoneContacts = [
+const LS_CONTACTS_KEY = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
