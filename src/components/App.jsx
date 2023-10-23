d1c018d889f108956d4d271e9c9c569fa31fed92
MartaShchur <m.matseyko199@gmail.com>
2023-10-23 13:44:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,25 +7,25 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
+const phoneContacts = [
+{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-const LS_CONTACTS_KEY = 'contacts';
 
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return loadLocalStorage(LS_CONTACTS_KEY) ?? [];
+    return loadLocalStorage(phoneContacts) ?? [];
   });
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    saveLocalStorage(LS_CONTACTS_KEY, contacts);
+    saveLocalStorage(phoneContacts, contacts);
   }, [contacts]);
 
-// const phoneContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
+
  
 // const LS_CONTACTS_KEY = 'contacts';
 // export const App = () => {
